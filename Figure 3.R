f1ecb39be9a75bd518cda19ee7edd8d85b459402
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-06-15 13:50:18 +0500

    Update Figure 3.R

--- Figure 3.R
+++ Figure 3.R
@@ -4,7 +4,7 @@ colors <- c("deeppink","green","red","blue", "yellow")
 M <- c("1-10","11-20","21-30","31-40","41-50",">50")
 
 barplot(t(mydata),names.arg=M, main=NA, ylab="Frequency", xlab="Length",beside=TRUE,cex.axis=1, cex.names=1, cex.lab=1.2,
-        col=colors, ylim=c(0,800), space=10)
+        col=colors, ylim=c(0,35))
 
 legend("topright", c("ABP","ACP","AFP","APP","AVP"), cex=1, 
        fill=colors)
