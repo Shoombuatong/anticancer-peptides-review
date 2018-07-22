bf926cdb37bced6e3f926d648980b2a58bf4ab73
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-22 09:21:31 +0500

    Update Figure_4.R

--- Figure_4.R
+++ Figure_4.R
@@ -22,8 +22,8 @@ space = "Lab") +
 theme(panel.grid.major = element_blank(),
   panel.border = element_blank(),
   panel.background = element_blank(),
-  legend.title = element_text(size = 13),
-        legend.text = element_text(size = 10),
+  legend.title = element_text(size = 15),
+        legend.text = element_text(size = 13),
         axis.title=element_text(size=15,face="bold"),
-        axis.text.x = element_text(angle = 360, vjust = 1, size = 11,hjust = 1),
-	axis.text.y = element_text(angle = 360, vjust = 1, size = 11,hjust = 1))
+        axis.text.x = element_text(angle = 360, vjust = 1, size = 16,hjust = 1),
+	axis.text.y = element_text(angle = 360, vjust = 1, size = 16,hjust = 1))
