b7a0727a6de0049f650e566e3453522c2be2a700
Shoombuatong2527 <33542325+Shoombuatong2527@users.noreply.github.com>
2018-07-22 09:21:58 +0500

    Update Figure_2.R

--- Figure_2.R
+++ Figure_2.R
@@ -1,8 +1,8 @@
-#### Bar Chart
 my_vector = c(2501, 214, 1066, 103, 180)
-
 names(my_vector)=c("ABP","ACP","AFP","APP","AVP")
 
-my_bar = barplot(my_vector,  ylim=c(0,2600), col= c("deeppink","green","red","blue", "yellow"), cex.axis=1, cex.names=0.9, cex.lab=1, cex.sub=1,
+my_bar = barplot(my_vector,  ylim=c(0,2600), col= c("deeppink","green","red","blue", "yellow"), cex.axis=1.3, cex.names=1.3, cex.lab=1.5,
     main="", space=0.7,
     ylab="Number of peptides")
+box();
+
